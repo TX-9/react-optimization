82a9334cda86c31ec0bb6ecea8a4a5e7c42a51cd
TX-9 <jacobleecs@gmail.com>
2021-09-09 14:55:06 -0400

    use useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from './components/UI/Button/Button';
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -7,15 +7,21 @@ function App() {
   const [ showDemo, setShowDemo ] = useState(false);
   console.log('App RUNNING!');
 
-  // brand new funtion evertime App is executed
-  const clickHandler = ()=> {
+  // store function cross funtion execution not to re-creat every execution
+  const clickHandler = useCallback(()=> {
     setShowDemo(prevShowDemo => !prevShowDemo);
-  };
+  }, []); // include any function, prop, context used in surounded funtion
+  // except state change function(setShowDemo) because React does not change that
+
+  // brand new funtion evertime App is executed
+  // const clickHandler = ()=> {
+  //   setShowDemo(prevShowDemo => !prevShowDemo);
+  // };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {/* React.memo(Button) not working because clickHandler is object */}
+      {/* Without useCallback() React.memo(Button) not working because clickHandler is object */}
       <Button onClick={clickHandler}>Toggle Demo</Button>
       {/* false is premitive */}
       <DemoOutput show={false}></DemoOutput>
