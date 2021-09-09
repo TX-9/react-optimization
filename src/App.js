7817fc22c2cda8b49289fabac83153055017a3f2
TX-9 <jacobleecs@gmail.com>
2021-09-09 14:59:33 -0400

    dependency

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [ showDemo, setShowDemo ] = useState(false);
+  const [ allowToggle, setAllowToggle ] = useState(false);
   console.log('App RUNNING!');
 
   // store function cross funtion execution not to re-creat every execution
   const clickHandler = useCallback(()=> {
-    setShowDemo(prevShowDemo => !prevShowDemo);
-  }, []); // include any function, prop, context used in surounded funtion
+    if (allowToggle) { // to work this line, allowToggle should be in dependency
+      setShowDemo(prevShowDemo => !prevShowDemo);
+    }
+  }, [allowToggle]); // include any function, prop, context used in surounded funtion
   // except state change function(setShowDemo) because React does not change that
 
   // brand new funtion evertime App is executed
@@ -18,13 +21,18 @@ function App() {
   //   setShowDemo(prevShowDemo => !prevShowDemo);
   // };
 
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {/* Without useCallback() React.memo(Button) not working because clickHandler is object */}
       <Button onClick={clickHandler}>Toggle Demo</Button>
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+      
       {/* false is premitive */}
-      <DemoOutput show={false}></DemoOutput>
+      <DemoOutput show={showDemo}></DemoOutput>
     </div>
   );
 }
